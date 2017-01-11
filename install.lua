3fa2b736ffa1e9022796cdc4ebc7c19eaf93b03f
Kardos Ferenc <kardos.ferenc5@gmail.com>
2017-01-11 14:22:27 +0100

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,27 +1,40 @@
 -- holopad installation script
 --
 --
-
-local libURL = "https://github.com/standal/Holopad/blob/master/lib/button.lua"
-local libURL = "https://github.com/standal/Holopad/blob/master/lib/holodeckicon.lua"
-local libURL = "https://github.com/standal/Holopad/blob/master/lib/touchpoint.lua"
-local startupURL = "https://github.com/standal/Holopad/blob/master/holopad.lua"
+ 
+local touchpointURL = "https://raw.githubusercontent.com/standal/Holopad/master/lib/touchpoint.lua"
+local buttonsURL = "https://raw.githubusercontent.com/standal/Holopad/master/lib/button.lua"
+local holodeckiconURL = "https://raw.githubusercontent.com/standal/Holopad/master/lib/holodeckicon.lua"
+local startupURL = "https://raw.githubusercontent.com/standal/Holopad/master/holopad.lua"
 local lib, startup
-local libFile, startupFile
-
-fs.makeDir("lib")
-
-lib = http.get(libURL)
-libFile = lib.readAll()
-
-local file1 = fs.open("lib/f", "w")
-file1.write(libFile)
-file1.close()
-
-startup = http.get(startupURL)
-startupFile = startup.readAll()
-
-
-local file2 = fs.open("startup", "w")
-file2.write(startupFile)
-file2.close()
+local touchpointFile, buttonsFile, holodeckiconFile, startupFile
+ 
+fs.makeDir ("lib")
+ 
+lib = http.get (touchpointURL)
+touchpointFile = lib.readAll ()
+ 
+local file1 = fs.open ("lib/touchpoint", "w")
+file1.write (touchpointFile)
+file1.close ()
+ 
+lib = http.get (buttonsURL)
+buttonsFile = lib.readAll ()
+ 
+local file2 = fs.open ("lib/buttons", "w")
+file2.write (buttonsFile)
+file2.close ()
+ 
+lib = http.get (holodeckiconURL)
+holodeckiconFile = lib.readAll ()
+ 
+local file3 = fs.open ("lib/holodeckicon", "w")
+file3.write (holodeckiconFile)
+file3.close ()
+ 
+startup = http.get (startupURL)
+startupFile = startup.readAll ()
+ 
+local file4 = fs.open ("startup", "w")
+file4.write (startupFile)
+file4.close ()
